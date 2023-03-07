4224209c08a40d58f0d99a65459ef95a9ff3d273
Unixan <115973884+Unixan@users.noreply.github.com>
2023-03-07 10:57:31 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Tegnteller
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var range = 250;
+            int maxPercent = 0;
+            var counts = new int[range];
+            string text = "something";
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                text = Console.ReadLine();
+                foreach (var character in text.ToUpper())
+                {
+                    maxPercent++;
+                    counts[(int)character]++;
+                }
+                for (var i = 0; i < range; i++)
+                {
+                    if (counts[i] > 0)
+                    {
+                        var character = (char)i;
+                        double percent = (100 / (double)maxPercent) * (double)counts[i];
+                        string print = character + " (" + percent.ToString("F1") + "%)";
+                        Console.CursorLeft = Console.BufferWidth - print.Length - 1;
+                        Console.WriteLine(print);
+                    }
+                }
+            }
+        }
+    }
+}
